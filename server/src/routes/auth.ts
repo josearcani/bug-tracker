9b59543ccbaa18798df2b804d5b484cf019ec37b
Juan Jose <juanjose.arcani2309@gmail.com>
2022-04-13 22:47:00 -0400

    feat: add validations for projects & auth routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
+import { check } from 'express-validator';
 import { login, signup, google, renewJwt } from '../controllers/auth';
 import { validateFields } from '../middlewares/validate-fields';
 import { validateJWT } from '../middlewares/validate-jwt';
 const router = Router();
 
 router.post('/login', login);
-router.post('/signup', signup);
+
+router.post('/signup', [
+  check('username').notEmpty(),
+  check('fName').notEmpty(),
+  check('lName').notEmpty(),
+  check('password').notEmpty(),
+  check('email').isEmail(),
+  validateFields
+], signup);
+
 router.post('/google', google);
 
 router.get('/',[
--- server/src/routes/projects.ts
+++ server/src/routes/projects.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { body, param } from 'express-validator';
 import { deleteProject, getProject, getProjects, postProject, putProject } from '../controllers/projects';
 import { validateFields } from '../middlewares/validate-fields';
 import { validateJWT } from '../middlewares/validate-jwt';
@@ -11,21 +12,26 @@ router.get('/', [
 
 router.get('/:projectId', [
   validateJWT,
+  param('projectId', 'projectID not valid').isUUID(),
   validateFields
 ], getProject);
 
 router.post('/', [
   validateJWT,
+  body('title').notEmpty(),
   validateFields
 ], postProject);
 
 router.put('/:projectId', [
   validateJWT,
+  param('projectId', 'projectID not valid').isUUID(),
+  body('title').notEmpty(),
   validateFields
 ], putProject);
 
 router.delete('/:projectId', [
   validateJWT,
+  param('projectId', 'projectID not valid').isUUID(),
   validateFields
 ], deleteProject);
 

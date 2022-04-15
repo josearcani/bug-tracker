8a91ad2b5efdb21b37a66754c0123b1816349c54
Juan Jose <juanjose.arcani2309@gmail.com>
2022-04-14 23:36:47 -0400

    feat: add validate priority middleware

---
+++ server/src/middlewares/validate-priority.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const validatePriority = ( ...priorites: string[] ) => {
+  return (req: Request, res:Response, next: NextFunction) => {
+    const { priority } = req.body;
+    if (!priorites.includes(priority)) {
+      return res.status(401).json({
+        message: `The priority needs to be ${priorites}`
+      })
+    }
+  }
+}

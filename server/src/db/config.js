d1e5aee3680a2d0ce06d5844f9412e14e0dee641
Juan Jose <juanjose.arcani2309@gmail.com>
2022-04-15 22:21:49 -0400

    deploy: bug-tracker 0.1.0

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -17,10 +17,16 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    "username": "xqxymkrjqkzara",
+    "password": "4f23f23da8bbeb21c61df4341a848e6ba4a4f8e6d174e948bb05f50931fc848d",
+    "database": "db2g3dbbkc6qtc",
+    "host": "ec2-52-73-155-171.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+          "require": true,
+          "rejectUnauthorized": false
+      }
+    }
   }
 }
